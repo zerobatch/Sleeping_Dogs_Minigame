a6c85d4c93eb14eaa0d8b4a0a5a2a6ea57da04b2
Zero <romerozeroguy@gmail.com>
2019-12-15 20:06:55 +0000

    Basic prototype to show game history with colors

--- index.js
+++ index.js
@@ -1,228 +1,269 @@
-const startGameform = document.getElementById('options')
+const startGameform = document.getElementById("options");
 
-startGameform.addEventListener('submit', function (event) {
-    event.preventDefault();
+startGameform.addEventListener("submit", function(event) {
+  event.preventDefault();
 
-    const difficulty = this.querySelector('select#difficulty').value;
+  const difficulty = this.querySelector("select#difficulty").value;
 
-    const gameInstance = new GameCore(difficulty);
-
-})
-
-
+  const gameInstance = new GameCore(difficulty);
+});
 
 class GameCore {
-    availableOptions = {
-        easy: {
-            items: 4,
-            tries: 6
-        },
-        normal: {
-            items: 7,
-            tries: 8
-        },
-        hard: {
-            items: 9,
-            tries: 10
-        },
+  availableOptions = {
+    easy: {
+      items: 4,
+      tries: 6
+    },
+    normal: {
+      items: 7,
+      tries: 8
+    },
+    hard: {
+      items: 9,
+      tries: 10
+    }
+  };
+
+  availableNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  gameHistory = [];
+
+  constructor(difficulty, gameUtils = GameUtils, gameContainer = null) {
+    this.difficulty = this.availableOptions[difficulty];
+    this.GameUtils = gameUtils;
+
+    this.gameNumbers = this.generateNumbersBasedOnDifficulty(
+      this.availableNumbers,
+      this.difficulty["items"]
+    );
+
+    this.mainContainer = document.createElement("main");
+    this.mainContainer.classList.add("main-game-container");
+
+    this.gameHistoryContainer = document.createElement("section");
+    this.gameHistoryContainer.setAttribute("id", "game-history");
+
+    this.gameContainer = gameContainer;
+
+    if (!this.gameContainer || !(this.gameContainer instanceof HTMLElement)) {
+      this.gameContainer = document.createElement("section");
+      this.gameContainer.setAttribute("id", "game");
     }
 
-    availableNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+    this.inputNumberContainer = this.createNumberContainer();
+    this.gameContainer.appendChild(this.inputNumberContainer);
 
-    gameHistory = []
+    this.mainContainer.appendChild(this.gameHistoryContainer);
+    this.mainContainer.appendChild(this.gameContainer);
 
-    constructor(difficulty, gameUtils = GameUtils, gameContainer = null, ) {
-        this.difficulty = this.availableOptions[difficulty];
-        this.GameUtils = gameUtils;
+    document.body.appendChild(this.mainContainer);
+  }
 
-        this.gameNumbers = this.generateNumbersBasedOnDifficulty(this.availableNumbers, this.difficulty['items']);
+  createNumberContainer() {
+    const container = document.createElement("div");
 
-        this.mainContainer = document.createElement('main');
-        this.mainContainer.classList.add('main-game-container');
+    container.classList.add("number-container");
 
-        this.gameHistoryContainer = document.createElement('section');
-        this.gameHistoryContainer.setAttribute('id', 'game-history');
+    for (let number = 0; number < this.difficulty["items"]; number++) {
+      const wrapper = this.createNumberWrapper(number);
 
-        this.gameContainer = gameContainer;
+      container.appendChild(wrapper);
+    }
 
-        if (!this.gameContainer || !(this.gameContainer instanceof HTMLElement)) {
-            this.gameContainer = document.createElement('section');
-            this.gameContainer.setAttribute('id', 'game');
+    const enterInputNumbers = document.createElement("button");
+    enterInputNumbers.classList.add("enter-input");
+    enterInputNumbers.innerHTML = "&times";
+
+    enterInputNumbers.addEventListener("pointerdown", event => {
+      const userInputResult = this.formatUserInputAsObject();
+      const userInputResultValuesAsArray = Object.values(
+        userInputResult
+      ).map(number => parseInt(number));
+
+      if (
+        this.GameUtils.haveDuplicateElementsOnArray(
+          userInputResultValuesAsArray
+        )
+      ) {
+        console.log("SE DUPLICAN NUMEROS");
+      } else {
+        this.compareUserInputWithGameNumbers(userInputResult);
+        this.updateGameHistory();
+      }
+    });
+
+    container.appendChild(enterInputNumbers);
+    return container;
+  }
+
+  updateGameHistory() {
+    console.log(this.gameNumbers);
+    this.GameUtils.emptyDomElement(this.gameHistoryContainer);
+
+    this.gameHistory.forEach(history => {
+      const wrapper = document.createElement("div");
+      wrapper.classList.add("history-container");
+
+      Object.keys(history).forEach(position => {
+        let stateClass = "";
+        const historyData = history[position];
+
+        if (!historyData.player_position) {
+          stateClass = "failed";
+        } else if (historyData.player_position === "exists") {
+          stateClass = "bad-position";
+        } else {
+          stateClass = "good-position";
         }
 
-        this.inputNumberContainer = this.createNumberContainer();
-        this.gameContainer.appendChild(this.inputNumberContainer);
+        const numberContainer = document.createElement("div");
+        numberContainer.classList.add("number-container", stateClass);
+        numberContainer.innerText = historyData.number;
 
-        this.mainContainer.appendChild(this.gameHistoryContainer);
-        this.mainContainer.appendChild(this.gameContainer);
+        wrapper.appendChild(numberContainer);
+      });
 
-        document.body.appendChild(this.mainContainer);
+      this.gameHistoryContainer.appendChild(wrapper);
+    });
+  }
 
+  createNumberWrapper(position) {
+    const wrapper = document.createElement("div");
+
+    wrapper.classList.add("number-wrapper");
+    wrapper.setAttribute("data-position", position);
+
+    for (let number = 0; number < 10; number++) {
+      const numberElement = document.createElement("div");
+      numberElement.setAttribute("class", `#${position}-${number}`);
+      numberElement.innerText = number;
+
+      wrapper.appendChild(numberElement);
     }
 
-    createNumberContainer() {
-        const container = document.createElement('div');
+    return wrapper;
+  }
 
-        container.classList.add('number-container');
+  compareUserInputWithGameNumbers(userInput) {
+    let gameHistoryObject = {};
 
-        for (let number = 0; number < this.difficulty['items']; number++) {
-            const wrapper = this.createNumberWrapper(number);
+    Object.keys(userInput).forEach(position => {
+      const userInputNumber = userInput[position];
 
-            container.appendChild(wrapper);
+      gameHistoryObject[position] = { ...this.gameNumbers[position] };
+      gameHistoryObject[position]["number"] = userInputNumber;
+      gameHistoryObject[position]["checked"] = true;
+
+      if (userInputNumber === this.gameNumbers[position]["number"]) {
+        gameHistoryObject[position]["player_position"] = position;
+      } else {
+        const numberExistInAnotherPositionResult = this.numberExistsOnGameNumbers(
+          userInputNumber
+        );
+        gameHistoryObject[position][
+          "player_position"
+        ] = numberExistInAnotherPositionResult ? "exists" : null;
+      }
+    });
+
+    this.gameHistory.push(gameHistoryObject);
+
+    return gameHistoryObject;
+  }
+
+  numberExistsOnGameNumbers(number) {
+    let result = false;
+
+    Object.keys(this.gameNumbers).forEach(position => {
+      if (number === this.gameNumbers[position]["number"]) {
+        result = true;
+      }
+    });
+
+    return result;
+  }
+
+  formatUserInputAsObject() {
+    const wrappers = this.inputNumberContainer.querySelectorAll(
+      ".number-wrapper"
+    );
+    const result = {};
+
+    Array.from(wrappers).forEach(wrapper => {
+      const position = wrapper.getAttribute("data-position");
+      let visibleNumber = 0;
+
+      Array.from(wrapper.children).forEach(number => {
+        const bounding = number.getBoundingClientRect();
+        if (bounding.top > 187 && bounding.top < 216) {
+          visibleNumber = number.innerText;
         }
+      });
 
-        const enterInputNumbers = document.createElement('button');
-        enterInputNumbers.classList.add('enter-input')
-        enterInputNumbers.innerHTML = '&times';
+      result[position] = parseInt(visibleNumber);
+    });
 
-        enterInputNumbers.addEventListener('pointerdown', (event) => {
-            const userInputResult = this.formatUserInputAsObject()
-            const userInputResultValuesAsArray = Object.values(userInputResult).map(number => parseInt(number))
+    return result;
+  }
 
-            if (this.GameUtils.haveDuplicateElementsOnArray(userInputResultValuesAsArray)) {
-                console.log("SE DUPLICAN NUMEROS")
-            } else {
-                this.compareUserInputWithGameNumbers(userInputResult);
-            }
+  generateNumbersBasedOnDifficulty(availableNumbers, items = 4) {
+    let random = {};
 
-        });
+    for (let index = 0; index < items; index++) {
+      const randomNumber = this.GameUtils.randomElementFromArray(
+        availableNumbers
+      );
 
-        container.appendChild(enterInputNumbers)
-        return container;
+      availableNumbers = availableNumbers.filter(number => {
+        return number !== randomNumber;
+      });
+
+      random[index] = {
+        number: randomNumber,
+        checked: false,
+        player_position: null
+      };
     }
 
-    compareUserInputWithGameNumbers(userInput) {
-        let gameHistoryObject = {}
-
-        Object.keys(userInput).forEach(position => {
-            const userInputNumber = userInput[position];
-
-            gameHistoryObject[position] = { ...this.gameNumbers[position] };
-            gameHistoryObject[position]['checked'] = true;
-
-            if (userInputNumber === this.gameNumbers[position]['number']) {
-                gameHistoryObject[position]['player_position'] = position;
-            } else {
-                const numberExistInAnotherPositionResult = this.numberExistsOnGameNumbers(userInputNumber);
-                gameHistoryObject[position]['player_position'] = numberExistInAnotherPositionResult ? 'exists' : null;
-            }
-        })
-
-        this.gameHistory.push(gameHistoryObject)
-
-        return gameHistoryObject;
-    }
-
-    numberExistsOnGameNumbers(number) {
-        let result = false;
-
-        Object.keys(this.gameNumbers).forEach(position => {
-            if (number === this.gameNumbers[position]['number']) {
-                result = true;
-            }
-        })
-
-        return result;
-    }
-
-    formatUserInputAsObject() {
-        const wrappers = this.inputNumberContainer.querySelectorAll('.number-wrapper');
-        const result = {}
-
-        Array.from(wrappers).forEach(wrapper => {
-            const position = wrapper.getAttribute('data-position');
-            let visibleNumber = 0;
-
-            Array.from(wrapper.children).forEach(number => {
-                const bounding = number.getBoundingClientRect();
-                if (bounding.top > 189 && bounding.top < 214) {
-                    visibleNumber = number.innerText;
-                }
-            })
-
-            result[position] = parseInt(visibleNumber);
-        })
-
-        return result;
-    }
-
-    createNumberWrapper(position) {
-        const wrapper = document.createElement('div');
-
-        wrapper.classList.add('number-wrapper');
-        wrapper.setAttribute('data-position', position);
-
-        for (let index = 0; index < 10; index++) {
-            const number = document.createElement('div');
-            number.innerText = index;
-
-            wrapper.appendChild(number);
-        }
-
-        return wrapper;
-    }
-
-
-    generateNumbersBasedOnDifficulty(availableNumbers, items = 4) {
-        let random = {};
-
-        for (let index = 0; index < items; index++) {
-            const randomNumber = this.GameUtils.randomElementFromArray(availableNumbers)
-
-            availableNumbers = availableNumbers.filter(number => {
-                return number !== randomNumber
-            });
-
-            random[index] = {
-                number: randomNumber,
-                checked: false,
-                player_position: null
-            };
-        }
-
-        return random;
-    }
+    return random;
+  }
 }
 
-
 class GameUtils {
+  static randomElementFromArray(array, num = 1) {
+    num = +num;
 
-    static randomElementFromArray(array, num = 1) {
-
-        num = +num;
-
-        if (isNaN(num) || num > array.length) {
-            return null
-        }
-
-        const length = array.length;
-
-        for (let i = length - 1; i > 0; i--) {
-            let j = Math.floor(Math.random() * (i + 1))
-
-            let tmp = array[j]
-
-            array[j] = array[i]
-            array[i] = tmp
-        }
-
-        const randomElement = num === 1 ? array[0] : array.slice(0, num);
-
-        return parseInt(randomElement);
+    if (isNaN(num) || num > array.length) {
+      return null;
     }
 
-    static haveDuplicateElementsOnArray(numbers) {
-        const originalLength = numbers.length;
+    const length = array.length;
 
-        let unique = Array.from(new Set(numbers));
+    for (let i = length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
 
-        return unique.length !== originalLength;
-    }
-    static emptyDomElement(element) {
-        const myNode = element;
-        while (myNode.firstChild) {
-            myNode.removeChild(myNode.firstChild);
-        }
+      let tmp = array[j];
+
+      array[j] = array[i];
+      array[i] = tmp;
     }
 
+    const randomElement = num === 1 ? array[0] : array.slice(0, num);
+
+    return parseInt(randomElement);
+  }
+
+  static haveDuplicateElementsOnArray(numbers) {
+    const originalLength = numbers.length;
+
+    let unique = Array.from(new Set(numbers));
+
+    return unique.length !== originalLength;
+  }
+  static emptyDomElement(element) {
+    const myNode = element;
+    while (myNode.firstChild) {
+      myNode.removeChild(myNode.firstChild);
+    }
+  }
 }
